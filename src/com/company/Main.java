49efb9ec0d12291ccb453ba36c183ea0494e86ff
guldan <gulum1111@gmail.com>
2022-05-14 14:07:40 -0400

    the updated commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("I am Groot");
+        System.out.println("I am Groot2 ");
+        System.out.println("I am Groot3");
+
+        System.out.println("I am a correct code");
+        System.out.println("I am not confused");
+
+
     }
 }
